d4fa8bc6d76e8f66d8de85c55fc970172cc699a5
andrepiress61 <andre.f.mesquita6@aluno.senai.br>
2025-08-07 09:32:13 -0300

    declarei variavies

---
+++ aula01/atividades.py
@@ -0,0 +1,4 @@
+Nome = "Andre Pires"
+Idade = 17
+Dt = "03 de maio 2008"
+print ("Olá meu nome é",Nome,"\nTenho",Idade,"anos","\nNasci em",Dt)
---
+++ aula01/variaveis.py
@@ -0,0 +1,8 @@
+
+#esse 10 representra quants vvezes vão aparecer no codigo, e a esse "*" significa multiplicação 
+print (10*"-","concatenando texto", 10*"-")
+#nome é uma variavel que se colocar em qualquer local ele vai aparecer no print, ou etc 
+nome = "Andre Pires"
+print ("Nome: ", nome)
+
+#Tipos de variaveis 
