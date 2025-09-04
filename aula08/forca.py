76e1bde6404e3edc7a01816b82ceff51f3dc44f4
andrepiress61 <andre.f.mesquita6@aluno.senai.br>
2025-09-04 11:36:15 -0300

    atividade

--- aula08/forca.py
+++ aula08/forca.py
@@ -3,118 +3,120 @@ import os
 import random
 import json
 
-# função para escolher a palavra baseada no tema
-def escolher_palavras(escolha):
-    # posiveis escolhas
-    if escolha == "1":
-        # encontra o arquivo json
-        with open ('palavras.json', 'r', encoding='utf-8') as f:
-            # salva o resultado em uma variavel
-            palavras = json.load(f)
-            # escolhe uma palavra na categoria animais
-            return random.choice(palavras["animais"])
+# loop principal
+while True:
+    # trata os erros
+    try:                 
+        # função de menu
+        def menu():
+            # variavel global
+            global escolha
 
-    elif escolha == '2':
-        # encontra o arquivo json
-        with open ('palavras.json', 'r', encoding='utf-8') as f:
-            # salva o resultado em uma variavel
-            palavras = json.load(f)
-            # escolhe uma palavra na categoria objetos
-            return random.choice(palavras["objetos"])
+            # menu
+            print(20*"-", "MENU FORCA", 20*"-")
+            print("1 - Animais ")
+            print("2 - Objetos da cozinha ")
+            print("3 - Comida ")
+            print('4 - Sair')
 
-    elif escolha == '3':
-        # encontra o arquivo json
-        with open ('palavras.json', 'r', encoding='utf-8') as f:
-            # salva o resultado em uma variavel
-            palavras = json.load(f)
-            # escolhe uma palavra na categoria comidas
-            return random.choice(palavras["comidas"])
-
-    elif escolha == "4":
-        print('Saindo...')
-        exit()
-
-    # se a escolha for invalida
-    else:
-        print('Opção invalida.')
-
-# função de jogar
-def jogar_forca():
-    # limpa tela
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-    # variavel global
-    global escolha
-
-    # a variavel palavra randomiza uma palavra na lista palavras
-    palavra = escolher_palavras(escolha)
-    letras_corretas =[]
-    letras_erradas =[]
-    tentativas = 6
-
-    # loop
-    while True:
-        # limpa tela
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-        # verifica se a letra escolhida foi a correta
-        palavra_escondida = ""
-        for letra in palavra:
-            if letra in letras_corretas:
-                palavra_escondida += letra 
-
-            else:
-                palavra_escondida += "_"
-
-        print('Palavra escondida', palavra_escondida)
-        print("Letras erradas",letras_erradas)
-        print("Tentativas", tentativas)
-
-        # se o usuario acertar a palavra
-        if palavra_escondida == palavra:
-            print("Voce ganhou ")
-            menu()
-            break
-
-        # se o usuario acabar com todas as tentativas
-        elif tentativas == 0:
-            print(f"Você perdeu,a palavra era {palavra}")
-            menu()
-            break
-
-        # usuario escolhe uma letra
-        letra_usuario = input("Digite uma letra ").lower()
-
-        # verifica se a letra escolhida pelo usuario ta na palavra
-        if letra_usuario in palavra:
-            print("Letra correta ")
-            letras_corretas.append(letra_usuario)
-
-        # se a letra escolhida nao esta na palavra
-        else:
-            print("Letra errada")
-            letras_erradas.append(letra_usuario)
-            tentativas -= 1
+            # usuario escolhe qual tema de forca
+            escolha = input('Escolha um dos temas: ')
             
-# função de menu
-def menu():
-    # variavel global
-    global escolha
+            # a variavel palavra randomiza uma palavra na lista palavras
+            palavra = escolher_palavras(escolha)
 
-    # menu
-    print(20*"-", "MENU FORCA", 20*"-")
-    print("1 - Animais ")
-    print("2 - Objetos da cozinha ")
-    print("3 - Comida ")
-    print('4 - Sair')
+            # chama a função de jogar
+            jogar_forca(palavra)
 
-    # usuario escolhe qual tema de forca
-    escolha = input('Escolha um dos temas: ')
-    
-    # chama a função de jogar
-    jogar_forca()
+        # função para escolher a palavra baseada no tema
+        def escolher_palavras(escolha):
+            # encontra o arquivo json
+            with open ('palavras.json', 'r', encoding='utf-8') as f:
+                # salva o resultado em uma variavel
+                palavras = json.load(f)
+            
+            # posiveis escolhas
+            if escolha == "1":
+                # escolhe uma palavra na categoria animais
+                return random.choice(palavras["animais"])
 
-if __name__ == "__main__":
-    # limpa tela
-    os.system('cls' if os.name == 'nt' else 'clear')
-    menu()
+            elif escolha == '2':
+                # escolhe uma palavra na categoria objetos
+                return random.choice(palavras["objetos"])
+
+            elif escolha == '3':
+                # escolhe uma palavra na categoria comidas
+                return random.choice(palavras["comidas"])
+
+            elif escolha == "4":
+                print('Saindo...')
+                exit()
+
+            # se a escolha for invalida
+            else:
+                print('Opção invalida.')
+
+        # função de jogar
+        def jogar_forca(palavra):
+            # limpa tela
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            # variavel global
+            global escolha
+
+            letras_corretas =[]
+            letras_erradas =[]
+            tentativas = 6
+
+            # loop
+            while True:
+                # limpa tela
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+                # verifica se a letra escolhida foi a correta
+                palavra_escondida = ""
+                for letra in palavra:
+                    if letra in letras_corretas:
+                        palavra_escondida += letra 
+
+                    else:
+                        palavra_escondida += "_"
+
+                print('Palavra escondida',palavra_escondida)
+                print("Letras erradas",letras_erradas)
+                print("Tentativas",tentativas)
+
+                # se o usuario acertar a palavra
+                if palavra_escondida == palavra:
+                    print("Voce ganhou ")
+                    menu()
+                    break
+
+                # se o usuario acabar com todas as tentativas
+                elif tentativas == 0:
+                    print(f"Você perdeu,a palavra era {palavra}")
+                    menu()
+                    break
+
+                # usuario escolhe uma letra
+                letra_usuario = input("Digite uma letra ").lower()
+
+                # verifica se a letra escolhida pelo usuario ta na palavra
+                if letra_usuario in palavra:
+                    print("Letra correta ")
+                    letras_corretas.append(letra_usuario)
+
+                # se a letra escolhida nao esta na palavra
+                else:
+                    print("Letra errada")
+                    letras_erradas.append(letra_usuario)
+                    tentativas -= 1
+
+        if __name__ == "__main__":
+            # limpa tela
+            os.system('cls' if os.name == 'nt' else 'clear')
+            menu()
+
+    except Exception as e:
+        print(f"Erro: {e}")
+        input('Pressione ENTER para continuar')
